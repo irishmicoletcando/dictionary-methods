ca9da531279ce97549867ee8acd998a8d4592050
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-12 14:09:57 +0800

    Deleted the defined functions and rearranged the code

--- dictionarymethods.py
+++ dictionarymethods.py
@@ -13,8 +13,25 @@
 # 				   The value is another dictionary of personal information
 # 		- Option 2: Search, ask full name then display the record
 # 		- Option 3: Ask the user if want to exit or retry.
-
 # declare dictionary of personal data
+
+  # if 3, exit or retry program
+  # ask the user if they want to exit or retry
+def exitOrTryAgain():
+  while True:
+    exitInput = input("Do you want to exit? (Y/N): ").lower()
+    # if retry display the menu of options and have the user selct an item in menu
+    if exitInput == "n":
+      # function name of the whole program -> will be inserted if both option 1 and 2 are done
+      print("ok") # for testing only
+    # if exit, exit the program
+    elif exitInput == "y": 
+      print("-------------------------------\nThank you!")
+      break
+    else:
+      print("\nEnter Y if you want to start again, or N to exit the program.")
+
+
 personal_data = {
   "fullName" : "Irish",
   "person1" : {
@@ -28,6 +45,9 @@ personal_data = {
     "Booster": "Yes",
     "Booster Number": "1st"
   }
+  "person2" : {
+    
+  }
 }
 
 # display menu of options
@@ -37,8 +57,10 @@ print("""
     2 -> Search
     3 -> Exit (y/n)
 """)
-# validating the user's input is its from 1 to 3
-def userChoice():
+
+
+while True:
+  # validating the user's input is its from 1 to 3
   while True:
     try:
       # ask user to select an item in the menu
@@ -51,10 +73,11 @@ def userChoice():
       print("Invalid! Please enter a number only.")
     else:
       break
-  return userChoiceMenuInput
-  
-def addingItems() :
-  # ask user for input and adding in dictionary variable
+    
+  # if 1
+  # if user choose 1, add the item on the dictionary 
+  if userChoiceMenuInput == 1:
+    # ask user for input and adding in dictionary variable
     personal_data["person2"] = {}
     # ask user for full name
     fullName = input("Enter your full name: ")
@@ -89,76 +112,7 @@ def addingItems() :
     # print saved if added
     print("Saved!")
 
-# main program
-def mainProgram() :
-  userChoiceInput = userChoice()
-  # if user choose 1, add the item on the dictionary 
-  if userChoiceInput == 1:
-    addingItems()
-    # # ask user for input and adding in dictionary variable
-    # personal_data["person2"] = {}
-    # # ask user for full name
-    # fullName = input("Enter your full name: ")
-    # personal_data["person2"]["Full Name"] = fullName
-    # # ask user for age
-    # age = int(input("Enter your age: "))
-    # personal_data["person2"]["Age"] = age
-    # # ask user for address
-    # address = input("Enter your address: ")
-    # personal_data["person2"]["Address"] = address
-    # # ask gender
-    # gender = input("Enter your gender: ")
-    # personal_data["person2"]["Gender"] = gender
-    # # ask phone number
-    # phonenumber = input("Enter your phone number: ")
-    # personal_data["person2"]["Phone Number"] = phonenumber
-    # # ask email
-    # email = input("Enter your email: ")
-    # personal_data["person2"]["Email"] = email
-    # # ask if fully vaccinated
-    # fullyvaccinated = input("Are you fully vaccinated? (Yes/No): ").lower()
-    # personal_data["person2"]["Fully Vaccinated"] = fullyvaccinated
-    # # ask if they have booster
-    # if fullyvaccinated == "yes":
-    #   booster = input("Do you have a booster? (Yes/No): ").lower()
-    #   personal_data["person2"]["Booster"] = booster
-    #   # if yes, ask how many 1st or 2nd
-    #   if booster == "yes":
-    #     boosternumber = input("Do you have 1st or 2nd booster?: ")
-    #     personal_data["person2"]["Booster Number"] = boosternumber
-    # print(personal_data.values())
-    # # print saved if added
-    # print("Saved!")
+  # if 2, check if the name is in the dictionary list
 
-
-# if 2
-# check if the name is in the dictionary list
-if userChoiceMenuInput == 2:
-  fullNameInOptionTwo = input("Enter your full name: ")
-  # if yes, display all the information
-  if fullNameInOptionTwo in personal_data["person1"]["Full Name"]:
-    print(f"Full Name: {fullNameInOptionTwo}")
-  # if no, print no records found
-  else:
-    print("No records found!")
-
-# if 3, exit or retry program
-# if userChoiceMenuInput == 3:
-#   exitOrTryAgain()
-# ask the user if they want to exit or retry
-def exitOrTryAgain():
-  while True:
-    exitInput = input("Do you want to exit? (Y/N): ").lower()
-    # if retry display the menu of options and have the user selct an item in menu
-    if exitInput == "n":
-      # function name of the whole program -> will be inserted if both option 1 and 2 are done
-      print("ok") # for testing only
-    # if exit, exit the program
-    elif exitInput == "y": 
-      print("-------------------------------\nThank you!")
-      break
-
-# if 3, exit or retry program
-if userChoiceMenuInput == 3:
-  exitOrTryAgain()
-  # will changed the arrangement after the program is finalized
+else: 
+  System.exit()
